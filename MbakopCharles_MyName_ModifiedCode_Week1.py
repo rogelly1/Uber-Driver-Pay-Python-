1ff5db00acef45a02f5789d4df86335f4c51da0c
rogelly1 <55643962+rogelly1@users.noreply.github.com>
2024-03-24 10:28:01 -0400

    Add files via upload

---
+++ MbakopCharles_MyName_ModifiedCode_Week1.py
@@ -0,0 +1,8 @@
+#variables
+Name= "Charles Mbakop"
+Class= "CMSC 105"
+Date= "03/18/2024"
+
+print ( "Hello Class!\n My name is:\t", Name)
+print ( "This class is:\n\t", Class + ".\n")
+print ( "Today's date is:\n\t", Date)
